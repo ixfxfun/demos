5e26e234042c0a8f8161b7f201d4ef1f749ed3cc
CH <clint@thestaticvoid.net>
2024-09-16 09:56:19 +0200

    Update espruino.js

--- io/espruino/mag/espruino.js
+++ io/espruino/mag/espruino.js
@@ -2,8 +2,13 @@
 // When calling 'magOn' we set the rate of the sensor, this can be one of these values (where higher == faster)
 //  80, 40, 20, 10, 5, 2.5, 1.25, 0.63, 0.31, 0.16 or 0.08.
 export const push = `
-Puck.magOn(2.5); 
+Puck.magOn(2.5);
+let skip = 3; // How many initial readings to ignore
 Puck.on('mag', xyz => {
+  if (skip > 0) {
+    skip--;
+    return;
+  }
   Bluetooth.println(JSON.stringify(xyz));
 });
 NRF.on("disconnect", () => reset());
@@ -11,9 +16,17 @@ NRF.on("disconnect", () => reset());
 
 export const poll = `
 Puck.magOn();
-setInterval(()=>Bluetooth.println(JSON.stringify(Puck.mag())), 100);
+let skip = 3; // How many initial readings to ignore
+setInterval(()=> {
+  let xyz = Puck.mag();
+  if (skip > 0) {
+    skip--;
+    return;
+  }
+  Bluetooth.println(JSON.stringify(xyz));
+}, 200);
 NRF.on('disconnect',()=> {
   Puck.magOff();
-  reset()
+  reset();
 });
 `;
